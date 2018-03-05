6ede0b4329e83bd1c41f0a7a880f5b9c23854701
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2018-03-05 23:48:10 +0100

    fix reading of strings near the end of a page

--- LogString/plugin.cpp
+++ LogString/plugin.cpp
@@ -5,9 +5,10 @@ bool pluginInit(PLUG_INITSTRUCT* initStruct)
 {
     _plugin_registerformatfunction(pluginHandle, "ascii", [](char* dest, size_t destCount, int argc, char* argv[], duint value, void* userdata)
     {
-        char string[MAX_STRING_SIZE / 2];
-        if(!DbgMemRead(value, string, sizeof(string) - 2))
+        if(!DbgMemIsValidReadPtr(value))
             return FORMAT_ERROR;
+        char string[MAX_STRING_SIZE / 2];
+        DbgMemRead(value, string, sizeof(string) - 2);
         if(strlen(string) >= destCount)
             return FORMAT_BUFFER_TOO_SMALL;
         strcpy_s(dest, destCount, string);
